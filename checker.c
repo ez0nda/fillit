8eb5ea9c288b03be22a5a5741fa13d189639d7e9
Emmanuel ZONDA <ezonda@e1r8p11.42.fr>
2018-12-04 17:11:02 +0100

    checker_ok

--- checker.c
+++ checker.c
@@ -6,22 +6,23 @@
 /*   By: ezonda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 14:36:41 by ezonda            #+#    #+#             */
-/*   Updated: 2018/12/03 17:23:49 by ezonda           ###   ########.fr       */
+/*   Updated: 2018/12/04 17:09:25 by ezonda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-#include <stdio.h>
 
 static int		ft_checkchar(char *tab)
 {
 	int i;
 	int dot;
 	int sharp;
+	int line_feed;
 
 	i = 0;
 	dot = 0;
 	sharp = 0;
+	line_feed = 0;
 	while (tab[i] != '\0')
 	{
 		if (tab[i] == '#')
@@ -29,18 +30,11 @@ static int		ft_checkchar(char *tab)
 		if (tab[i] == '.')
 			dot++;
 		if (tab[i] != '#' && tab[i] != '.' && tab[i] != ' ')
-		{
-			printf("error : wrong char\n");
 			return (-1);
-		}
 		i++;
 	}
 	if (sharp != 4 || dot != 12)
-	{
-		printf("error : wrong number of |#| or |.|\n");
 		return (-1);
-	}
-	printf("chars ok\n");
 	return (0);
 }
 
@@ -56,26 +50,45 @@ static int		ft_checkline(char *tab)
 		if (tab[i] == '.' || tab[i] == '#')
 			len++;
 		if (len > 4)
-		{
-			printf("error : line > 4 chars\n");
 			return (-1);
-		}
 		if (len == 4 && tab[len] == ' ')
 			len = 0;
 		i++;
 	}
-	printf("line_size ok\n");
 	return (0);
+}
+
+static int		ft_checktetri(char *tab)
+{
+	int i;
+	int occ;
+
+	i = 0;
+	occ = 0;
+	while (tab[i])
+	{
+		if (tab[i] == '#' && tab[i + 1] == '#')
+			occ++;
+		if (tab[i] == '#' && tab[i + 5] == '#')
+			occ++;
+		if (tab[i] == '#' && tab[i - 1] == '#')
+			occ++;
+		if (tab[i] == '#' && tab[i - 5] == '#')
+			occ++;
+		i++;
+	}
+	if (occ == 6 || occ == 8)
+		return (0);
+	return (-1);
 }
 
 int		ft_checkall(char *tab)
 {
-	printf("tetri : %s\n", tab);
 	if (ft_checkchar(tab) == -1)
 		return (-1);
 	if (ft_checkline(tab) == -1)
 		return (-1);
-//	if (ft_checktetri(tab) == 0)
-//		return (0);
+	if (ft_checktetri(tab) == -1)
+		return (-1);
 	return (0);
 }
